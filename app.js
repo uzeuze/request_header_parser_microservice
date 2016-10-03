89cccbf8f8026ba0e32cec2e2a83e4c33fe03f5e
Egemen Can Uze <egemencanuze@gmail.com>
2016-10-03 11:48:09 +0300

    Change json response

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ app.set("port", process.env.PORT || 3000);
 
 app.get('/', function(req,res){
   var header = req.headers;
+  var ip = header['x-forwarded-for'] ||
+     req.connection.remoteAddress;
   res.json({
-    'ipaddress': req.ip,
-    'language': header['accept-language'],
-    'software': header['user-agent']
+    'ipaddress': ip,
+    'language': header['accept-language'].split(',')[0],
+    'software': header['user-agent'].split(') ')[0].split(' (')[1]
   });
 });
 
